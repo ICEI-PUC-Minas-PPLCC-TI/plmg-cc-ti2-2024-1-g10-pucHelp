ea41459a9c65889cb8b227ef059d922451cc8ff2
Joao Victor Martins <joaovictoranjost3@gmail.com>
2024-05-17 02:15:52 -0300

    Commit final.

--- Codigo/src/main/resources/script-bd/puchelp.sql
+++ Codigo/src/main/resources/script-bd/puchelp.sql
@@ -1,134 +1,86 @@
--- Criar a database PUCHELP
-CREATE DATABASE IF NOT EXISTS PUCHELP;
-
--- Usar a database PUCHELP
-USE PUCHELP;
-
--- Criação da tabela de tipos de área
-CREATE TABLE tipo_area (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(50) UNIQUE
+-- Criação da tabela area
+CREATE TABLE IF NOT EXISTS area (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50)
 );
 
-select * from tipo_area ta;
-
--- Inserção dos tipos de área
-INSERT INTO tipo_area (nome) VALUES ('humanas'), ('exatas'), ('biológicas');
-
-
-CREATE SEQUENCE area_id_seq;
-
-
--- Criar tabela area
-CREATE TABLE area (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(50),
-    tipo INT,
-    foreign key (tipo) references tipo_area(id)
+-- Criação da tabela tipo_usuario
+CREATE TABLE IF NOT EXISTS tipo_usuario (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(20)
 );
 
--- Criar tabela curso
-CREATE TABLE curso (
-    id SERIAL PRIMARY KEY,
-    disciplinas VARCHAR(100),
-    idArea INT,
-    FOREIGN KEY (idArea) REFERENCES area(id)
-);
-
--- Criar tabela DisciplinasCurso
-CREATE TABLE DisciplinasCurso (
-    id SERIAL PRIMARY KEY,
-    idCurso INT,
-    nomeDisciplina VARCHAR(100),
-    FOREIGN KEY (idCurso) REFERENCES curso(id)
-);
-
--- Criar tabela professor
-CREATE TABLE professor (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    materia VARCHAR(100),
-    idCurso INT,
-    FOREIGN KEY (idCurso) REFERENCES curso(id)
-);
-
--- Criar tabela CursoProfessores
-CREATE TABLE CursoProfessores (
-    id SERIAL PRIMARY KEY,
-    idCurso INT,
-    idProfessor INT,
-    FOREIGN KEY (idCurso) REFERENCES curso(id),
-    FOREIGN KEY (idProfessor) REFERENCES professor(id)
-);
-
--- Criar tabela aluno
-CREATE TABLE aluno (
-    id SERIAL PRIMARY KEY,
-    matricula VARCHAR(20),
-    idCurso INT,
-    periodo INT,
-    nome VARCHAR(100),
-    FOREIGN KEY (idCurso) REFERENCES curso(id)
-);
-
-create table tipo_publicacao (
-	id SERIAL primary key,
+-- Criação da tabela curso
+CREATE TABLE IF NOT EXISTS curso (
+	id SERIAL PRIMARY KEY,
+	idarea INTEGER REFERENCES area(id),
 	nome VARCHAR(100)
 );
 
--- Criar tabela publicações
-CREATE TABLE publicacoes (
-    id SERIAL PRIMARY KEY,
-    idTipo INT,
-    conteudo TEXT,
-    idAluno INT,
-    FOREIGN KEY (idAluno) REFERENCES aluno(id),
-    FOREIGN KEY (idTipo) REFERENCES tipo_publicacao(id)
+-- Criação da tabela disciplinascurso
+CREATE TABLE IF NOT EXISTS disciplinascurso (
+	id SERIAL PRIMARY KEY,
+	idcurso INTEGER REFERENCES curso(id),
+	nomedisciplina VARCHAR(100)
 );
 
-create table RelacionamentoAlunos (
-	id SERIAL primary key,
-	idAluno INT,
-	idAlunoSeguido INT,
-	foreign key (idAluno) references aluno(id),
-	foreign key (idAlunoSeguido) references aluno(id)
+-- Criação da tabela usuario
+CREATE TABLE IF NOT EXISTS usuario (
+	id SERIAL PRIMARY KEY,
+	cpf VARCHAR(100),
+	matricula INTEGER,
+	tipo INTEGER REFERENCES tipo_usuario(id),
+	idcurso INTEGER REFERENCES curso(id),
+	periodo INTEGER,
+	nome VARCHAR(100),
+	senha VARCHAR(30)
 );
 
-create table tipo_usuario (
- id SERIAL primary key,
- nome VARCHAR(20)
+-- Criação da tabela aluno
+CREATE TABLE IF NOT EXISTS aluno (
+	id SERIAL PRIMARY KEY,
+	matricula VARCHAR(20),
+	idcurso INTEGER REFERENCES curso(id),
+	periodo INTEGER,
+	nome VARCHAR(100)
+);
+
+-- Criação da tabela publicacoes
+CREATE TABLE IF NOT EXISTS publicacoes (
+	id SERIAL PRIMARY KEY,
+	tipo VARCHAR(100),
+	conteudo TEXT,
+	idaluno INTEGER REFERENCES usuario(id),
+	likes INTEGER,
+	coments TEXT
 );
 
 
-create table Usuario (
- id SERIAL primary key,
- cpf VARCHAR(100),
- matricula INT,
- tipo INT,
- idCurso INT,
- periodo INT,
- nome VARCHAR(100),
- foreign key (idCurso) references curso(id)
-);
+-- Adiciona a chave estrangeira na tabela curso
+ALTER TABLE curso
+ADD CONSTRAINT fk_curso_area
+FOREIGN KEY (idarea) REFERENCES area(id);
 
-select * from tipo_usuario;
+-- Adiciona a chave estrangeira na tabela disciplinascurso
+ALTER TABLE disciplinascurso
+ADD CONSTRAINT fk_disciplinascurso_curso
+FOREIGN KEY (idcurso) REFERENCES curso(id);
 
+-- Adiciona a chave estrangeira na tabela usuario
+ALTER TABLE usuario
+ADD CONSTRAINT fk_usuario_tipo_usuario
+FOREIGN KEY (tipo) REFERENCES tipo_usuario(id);
 
-ALTER TABLE usuario 
-ADD COLUMN  senha VARCHAR(30);
+ALTER TABLE usuario
+ADD CONSTRAINT fk_usuario_curso
+FOREIGN KEY (idcurso) REFERENCES curso(id);
 
-alter table usuario 
-alter column cpf TYPE VARCHAR(100);
+-- Adiciona a chave estrangeira na tabela aluno
+ALTER TABLE aluno
+ADD CONSTRAINT fk_aluno_curso
+FOREIGN KEY (idcurso) REFERENCES curso(id);
 
-select * from usuario ;
-select * from area a ;
-select * from curso c;
-
-insert into public.curso (d)
-
-select * from usuario u;
-
-insert into tipo_usuario (nome) values ('Administrador');
-select * from curso c;
-
-
+-- Adiciona a chave estrangeira na tabela publicacoes
+ALTER TABLE publicacoes
+ADD CONSTRAINT fk_publicacoes_usuario
+FOREIGN KEY (idaluno) REFERENCES usuario(id);
